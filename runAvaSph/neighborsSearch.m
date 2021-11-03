233ecf710fbacbd3126bb4495f1eb01bc63b64dd
belieresfrendo <amaury.belieres---frendo@ensta-paris.fr>
2021-11-03 22:49:45 +0100

    begin of neighborsSearch function. Need to think carefully what we are doing about

--- runAvaSph/neighborsSearch.m
+++ runAvaSph/neighborsSearch.m
@@ -1,21 +1,72 @@
-function nNeighbors, neighbors = neighborsSearch(particlesPosition, dem)
-    """ Get the neighbors of each particle for SPH computation
-    
-    Parameters
-      ----------
-      particlesPosition : float
-          position of the particle
-      dem: ( x 2) array
-          digital elevation model
-    Returns
-      -------
-      nNeighbors : int
-          number of neighbors
-      neighborsData : (nNeighbors) array
-          n° of each neighbor of the particle
-          particles position: float (longitudinal coordinate)
-          particle speed: float (longitudinal coordinate)
-          particles cell number: float
-          particles mass: float
-    """
+function nNeighbors, neighbors = ...
+                 neighborsSearch(particlePosition, particleCell, nParticles,
+                 particlesCellArray, particlesPositionArray, dem, nCell, rKernel)
+
+%==============================================================================%                 
+%         GET THE NEIGHBORS OF A GIVEN PARTICLE FOR SPH COMPUTATION            %
+%                                                                              %
+% Parameters                                                                   %
+% ----------                                                                   %
+%   particlePosition: float                                                    %
+%     position of the particle                                                 %
+%   particleCell: int                                                          %
+%     number of the particle cell                                              %
+%   nParticles: int                                                            %
+%     number of particles
+%   particlesCellArray: (nParticles x 1) Array                                  %
+%     particles cell number: int - each particle cell number                    %
+%   particlesCellPosition: (nParticles x 1) Array                               %
+%     particles cell position: int - each particle cell longitudinal coordinate %
+%   dem: ( nCell x ... ) array                                                 %
+%     digital elevation model                                                  %
+%   nCell: int                                                                 %
+%     number of cells                                                          %
+%   rKernel: int                                                               %
+%     smoothing lenght, and cell size                                          %
+% Returns                                                                      %
+% -------                                                                      %
+%   nNeighbors : int                                                           %
+%     number of neighbors                                                      %
+%   neighborsData : (nNeighbors) array                                         %
+%     n° of each neighbor of the particle                                      %
+%     particles position: float (longitudinal coordinate)                      %
+%     particle speed: float (longitudinal coordinate)                          %
+%     particles cell number: float                                             %
+%     particles mass: float                                                    %
+%==============================================================================%
+
+  nNeighbors=0
+  % if the particle isn't on boundaries
+  if (particleCell!=1 & particleCell!=nCell)
+    for l=1:nParticles
+      particlesCellArrayl = particlesCellArray(l)
+      if (particleCell==particlesCellArrayl | ...
+          particleCell==particlesCellArrayl + 1 | ...
+          particleCell==particlesCellArrayl - 1) & l != particleNumber
+        nNeighbors ++
+      endif
+    endfor %l
+  endif
+  % if the particle is on boundaries
+  if particleCell==1
+    for l=1:nParticles
+      particlesCellArrayl = particlesCellArray(l)
+      if (particleCell==particlesCellArrayl | ...
+          particleCell==particlesCellArrayl + 1 | ...
+          particleCell==particlesCellArrayl - 1)
+        nNeighbors ++
+      endif
+    endfor %l
+  endif
+  if (particleCell!=1 & particleCell!=nCell)
+    for l=1:nParticles
+      particlesCellArrayl = particlesCellArray(l)
+      if (particleCell==particlesCellArrayl | ...
+          particleCell==particlesCellArrayl + 1 | ...
+          particleCell==particlesCellArrayl - 1)
+        nNeighbors ++
+      endif
+    endfor %l
+  endif
+  
 endfunction
