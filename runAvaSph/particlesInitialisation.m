c209859f53d6f49add736611988b40a329bc8fd2
belieresfrendo <amaury.belieres---frendo@ensta-paris.fr>
2021-11-05 20:54:55 +0100

    updating the particle data

--- runAvaSph/particlesInitialisation.m
+++ runAvaSph/particlesInitialisation.m
@@ -1,25 +1,38 @@
 function particlesData = particlesInitialisation(nPart, mTot, releaseArea, dem)
-    """ Initialize the particles mass and position
-        Uniform distribution in the release area
-      
-    Parameters
-      ----------
-      nPart : float
-          number of particles
-      mTot: float
-          initial total mass of the avalanche
-      releaseArea: floats ????               /!\ to be carrefully defined /!\
-          coordinates of begin and end of the release area
-    Returns
-      -------
-      particlesData: ( x nPart) array
-          1. particles position: float (longitudinal coordinate)
-          2. particle speed: float (longitudinal coordinate)
-          3. particle acceleration accross X coordinate: float
-          4. particle acceleration accross Z coordinate: float
-          5. particles cell number: float
-          6. angle of the slope: float
-          7. particles mass: float
-    """
+
+%===============================================================================
+%   Initialize the particles mass and position                                 %
+%       Uniform distribution in the release area                               %
+%                                                                              %
+%   Parameters                                                                 %
+%   ----------                                                                 %
+%     nPart : float                                                            %
+%         number of particles                                                  %
+%     mTot: float                                                              %
+%         initial total mass of the avalanche                                  %
+%     releaseArea: floats ????               /!\ to be carrefully defined /!\  %
+%         coordinates of begin and end of the release area                     %
+%   Returns                                                                    %
+%   -------                                                                    %
+%     particlesData: ( x nPart) array                                          %
+%         1. xArray: nPart float array                                         %
+%             array with particle abscix                                       %
+%         2. zArray: nPart float array                                         %
+%             array with particle ordonate                                     %
+%         3. longitudinalArray: nPart float array                              %
+%             array with particle longitudinal coordinate                      %
+%         4. uxArray: nPart float array                                        %
+%             x component of each particle speed                               %
+%         5. uzArray: nPart float array                                        %
+%             z component of each particle speed                               %
+%         6. axArray: nPart float array                                        %
+%             x component of each particle acceleration                        %
+%         7. azArray: nPart float array                                        %
+%             y component of each particle acceleration                        %
+%         8. cellArray: nPart int array                                        %
+%             cell number of each particle                                     %
+%         9. massArray: nPart float array                                      %
+%             mass of each particle                                            %
+%===============================================================================
     
 endfunction
